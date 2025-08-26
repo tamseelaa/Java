5e364e0d905109a1901b48772d23a9dcf96674f4
tamseelaa <tamseelm@metropolia.fi>
2025-08-26 09:14:39 +0300

    Solving

--- Modules/src/week1/three/QuadEqSolver.java
+++ Modules/src/week1/three/QuadEqSolver.java
@@ -6,11 +6,11 @@ public class QuadEqSolver {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Quadratic Equation:      Ax^2 + Bx + C = 0");
-        System.out.print("Enter The Value for *A* coefficient in the equation");
+        System.out.print("Enter The Value for *A* coefficient in the equation: ");
         double A = Double.parseDouble(scanner.nextLine());
-        System.out.print("Enter the Value for *B* coefficient in the equation");
+        System.out.print("Enter the Value for *B* coefficient in the equation: ");
         double B = Double.parseDouble(scanner.nextLine());
-        System.out.print("Enter the Value for *C* coefficient in the equation");
+        System.out.print("Enter the Value for *C* coefficient in the equation: ");
         double C = Double.parseDouble(scanner.nextLine());
 
         double Value = (B*B) - (4*A*C);
@@ -18,11 +18,11 @@ public class QuadEqSolver {
         if (Value>0){
             double x1 = (-B + Math.sqrt(Value))/(2*A);
             double x2 = (-B - Math.sqrt(Value))/(2*A);
-            System.out.printf("The roots are x1: %.2f and x2: %.2f%n", x1,x2);
+            System.out.printf("The roots are different and real: x1: %.2f and x2: %.2f%n", x1,x2);
         }
         else if(Value==0){
             double x= -B/(2*A);
-            System.out.printf("The roots were same and real: %.2f%n",x);
+            System.out.printf("The root is real: %.2f%n",x);
         }
         else{
             System.out.println("There is No Real Root for the equation");

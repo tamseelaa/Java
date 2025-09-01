2289d3261c0392821e4b2142800723c56d591b6f
tamseelaa <tamseelm@metropolia.fi>
2025-09-01 03:23:53 +0300

    Solving

---
+++ ProjectPrep/src/W1/Customer.java
@@ -0,0 +1,51 @@
+package W1;
+
+public class Customer {
+    private static int nextId = 1;
+    private final int id;
+    private long startTime;
+    private long endTime;
+    private Long serviceStartTime;
+    private Long serviceEndTime;
+    private Long serviceDurationNs;
+
+    public Customer() {this.id = nextId++;}
+
+    public Customer(long startTime) {
+        this();
+        this.startTime = startTime;}
+
+    public int getId() {return id;}
+
+    public long getStartTime() {return startTime;}
+
+    public void setStartTime(long startTime) {this.startTime = startTime;}
+
+    public long getEndTime() {return endTime;}
+
+    public void setEndTime(long endTime) {this.endTime = endTime;}
+
+    public long timeSpent() {return endTime - startTime;}
+
+    public void markServiceStart(long t) {this.serviceStartTime = t;}
+
+    public void markServiceEnd(long t) {
+        this.serviceEndTime = t;
+        if (serviceStartTime != null) {
+            this.serviceDurationNs = serviceEndTime - serviceStartTime;
+        }
+    }
+
+    public Long getServiceDurationNs() {
+        return serviceDurationNs;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}

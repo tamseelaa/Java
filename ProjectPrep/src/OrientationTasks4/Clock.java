507b983099c8207e40e4457f053b2f039be11b44
tamseelaa <tamseelm@metropolia.fi>
2025-09-05 22:45:45 +0300

    Solving

---
+++ ProjectPrep/src/OrientationTasks4/Clock.java
@@ -0,0 +1,44 @@
+package OrientationTasks4;
+
+public class Clock {
+    private static Clock instance;
+    private double currentTime;
+
+    private Clock() {
+        this.currentTime = 0.0;
+    }
+
+    public static synchronized Clock getInstance() {
+        if (instance == null) {
+            instance = new Clock();
+        }
+        return instance;
+    }
+
+    public void setTime(double t) {
+        this.currentTime = t;
+    }
+
+    public double getTime() {
+        return currentTime;
+    }
+
+    public void advance(double delta) {
+        this.currentTime += delta;
+    }
+}
+
+// Test program
+class ClockTest {
+    public static void main(String[] args) {
+        Clock c = Clock.getInstance();
+        System.out.println("Time = " + c.getTime());
+        c.advance(5);
+        System.out.println("Time = " + c.getTime());
+        c.setTime(42);
+        System.out.println("Time = " + c.getTime());
+
+        Clock c2 = Clock.getInstance();
+        System.out.println("Same instance? " + (c == c2));
+    }
+}

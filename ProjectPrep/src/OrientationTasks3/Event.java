1a8b2fc9d61ada98670680d5a87d3639d4316d95
tamseelaa <tamseelm@metropolia.fi>
2025-09-15 10:46:39 +0300

    Solving

--- ProjectPrep/src/OrientationTasks3/Event.java
+++ ProjectPrep/src/OrientationTasks3/Event.java
@@ -7,7 +7,7 @@ class Event {
     public Event(String type, double time) {
         this.type = type;
         this.time = time;
-    }
+    }//
 
     public String toString() {
         return String.format("Event[%s @ %.2f]", type, time);

78fb5e2f2b078550bdc086420e80c3daa40a61c0
tamseelaa <tamseelm@metropolia.fi>
2025-09-30 22:14:16 +0300

    Solving

--- module_7_1/src/main/java/datasource/MariaDbConnection.java
+++ module_7_1/src/main/java/datasource/MariaDbConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class MariaDbConnection {
     private static final String URL = "jdbc:mariadb://localhost:3306/currency_db";
-    private static final String USER = "roots";       // your DB user
-    private static final String PASSWORD = "Shahid75"; // your DB password
+    private static final String USER = "root";       // your DB user
+    private static final String PASSWORD = "password"; // your DB password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
